7e5a1fece11842ed23a103fe0cce1e2e64995c87
Gordey <gordeylog71@gmail.com>
2023-01-10 16:08:12 +0300

    test

---
+++ script.js
@@ -0,0 +1,15 @@
+let youName,
+    youLastName,
+    youAge;
+
+youName = prompt('Ваше имя?');
+youLastName = prompt('Ваша фамилия?');
+youAge = prompt('Сколько вам лет?');
+
+alert('Ваше имя ' + youName);
+alert('Ваша фамилия ' + youLastName);
+alert('Вам ' + youAge + ' лет');
+
+console.log('Ваше имя ' + youName);
+console.log('Ваша фамилия ' + youLastName);
+console.log('Вам ' + youAge + ' лет');
